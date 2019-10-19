2e99e1678197d951d5acbb6ed06648b8ce291264
parkgeonhu <parkgeonhu@gmail.com>
2019-10-19 07:36:47 +0000

    add total_price on shoppingList

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hello">
         <el-row>
-            <el-col :span="8" v-for="(o, index) in 3" :key="o" :offset="index > 0 ? 2 : 0">
+            <el-col :span="8" v-for="(o, index) in 4" :key="o" :offset="index > 0 ? 2 : 0">
                 <el-card :body-style="{ padding: '0px' }">
                     <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
                     <div style="padding: 14px;">
@@ -15,7 +15,8 @@
         </el-row>
 		<br>
 		<br>
-        <el-table :data="products" style="width: 100%;" @current-change="selectProduct">
+		<div id="cafe-menu">
+        <el-table :data="products" style="width: 100%;" @row-click="selectProduct">
             <div>
                 <el-table-column align=center header-align=center prop="default_price" label="가격">
                 </el-table-column>
@@ -23,19 +24,10 @@
                 </el-table-column>
             </div>
         </el-table>
-
-		<!-- <div>
-			<el-table :data="p_storage" style="width: 100%;">
-            <div>
-
-                <el-table-column align=center header-align=center prop="default_price" label="가격">
-                </el-table-column>
-                <el-table-column align=center header-align=center prop="name" label="담긴 상품">
-                </el-table-column>
-            </div>
-        </el-table>
-		</div> -->
-		<el-table :data="p_storage" style="width: 100%;" @current-change="selectProduct">
+	</div>
+	<br><br>
+		<div id="shopping-storage">
+		<el-table :data="p_storage" style="width: 100%;">
             <div>
                 <el-table-column align=center header-align=center prop="default_price" label="가격">
                 </el-table-column>
@@ -43,7 +35,10 @@
                 </el-table-column>
             </div>
         </el-table>
-		
+	</div>
+		<div>
+			총 가격 : {{total_price}}
+	</div>
 		<el-button v-on:click="orderProduct">주문하기</el-button>
 		
 		
@@ -66,14 +61,19 @@ export default {
         return {
             products: null,
 			val : "카페이름@제품",
-			p_storage : []
+			p_storage : [],
+			total_price : 0
         }
     },
     mounted() {
         this.get_menu()
     },
     methods: {
+		get_total_price : function(){
+		},
         selectProduct: function(val) {
+			this.total_price+=val.default_price
+			console.log("clicked")
             this.p_storage.push({
                 "productUUID": val.productUUID,
 				"name" : val.name,
